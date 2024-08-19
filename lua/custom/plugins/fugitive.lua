62408cac10046ec35db4927b090352e692c1054e
misdirection <misdirection@live.de>
2024-08-19 21:41:57 +0200

    Fugitive added

---
+++ lua/custom/plugins/fugitive.lua
@@ -0,0 +1,7 @@
+return {
+  'tpope/vim-fugitive',
+  config = function()
+    -- Hier kannst du weitere Konfigurationen vornehmen, falls nötig.
+    -- Zum Beispiel könntest du Keybindings für Fugitive hinzufügen.
+  end,
+}
