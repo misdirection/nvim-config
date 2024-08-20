c33a7afb9b9e22f3243cb3756244bc156fcb200b
misdirection <misdirection@live.de>
2024-08-20 21:14:22 +0200

    debug keybinds

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -33,7 +33,7 @@ return {
       { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
       { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
       { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
-      { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+      { '<leader>bb', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
       {
         '<leader>B',
         function()
