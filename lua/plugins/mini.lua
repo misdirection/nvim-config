1caa2c01276dcfea1c770d31b7cf86f8765a13f2
misdirection <misdirection@live.de>
2024-08-29 22:16:16 +0200

    overhaul part 2

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,13 @@
+return { -- Collection of various small independent plugins/modules
+  'echasnovski/mini.nvim',
+  config = function()
+    require('mini.ai').setup { n_lines = 500 }
+    local statusline = require 'mini.statusline'
+    statusline.setup { use_icons = vim.g.have_nerd_font }
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
+  end,
+}
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,12 +1,7 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {},
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-    'rcarriga/nvim-notify',
-  },
-  require('noice').setup {
+  opts = {
     lsp = {
       override = {
         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
@@ -15,11 +10,14 @@ return {
       },
     },
     presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
+      bottom_search = false, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
     },
+  },
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
   },
 }
