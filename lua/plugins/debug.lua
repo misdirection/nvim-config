8b99b178a3a758988f46862a3b5bef43a6ef2eb7
misdirection <misdirection@live.de>
2024-09-12 21:23:23 +0200

    debug config

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,60 +1,84 @@
 return {
---   'mfussenegger/nvim-dap',
---   dependencies = {
---     'rcarriga/nvim-dap-ui',
---     'nvim-neotest/nvim-nio',
---     'williamboman/mason.nvim',
---     'jay-babu/mason-nvim-dap.nvim',
---   },
---   keys = function(_, keys)
---     local dap = require 'dap'
---     local dapui = require 'dapui'
---     return {
---       { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
---       { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
---       { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
---       { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
---       { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
---       {
---         '<leader>B',
---         function()
---           dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
---         end,
---         desc = 'Debug: Set Breakpoint',
---       },
---       { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
---       unpack(keys),
---     }
---   end,
---   config = function()
---     local dap = require 'dap'
---     local dapui = require 'dapui'
---
---     require('mason-nvim-dap').setup {
---       automatic_installation = true,
---       handlers = {},
---       ensure_installed = {
---       },
---     }
---     dapui.setup {
---       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
---       controls = {
---         icons = {
---           pause = '⏸',
---           play = '▶',
---           step_into = '⏎',
---           step_over = '⏭',
---           step_out = '⏮',
---           step_back = 'b',
---           run_last = '▶▶',
---           terminate = '⏹',
---           disconnect = '⏏',
---         },
---       },
---     }
---
---     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
---     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
---     dap.listeners.before.event_exited['dapui_config'] = dapui.close
---   end,
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+    'theHamsta/nvim-dap-virtual-text',
+    'mfussenegger/nvim-dap-python'
+  },
+  keys = function(_, keys)
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+    return {
+      { '<F5>',      dap.continue,          desc = 'Debug: Start/Continue' },
+      { '<F1>',      dap.step_into,         desc = 'Debug: Step Into' },
+      { '<F2>',      dap.step_over,         desc = 'Debug: Step Over' },
+      { '<F3>',      dap.step_out,          desc = 'Debug: Step Out' },
+      { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+      {
+        '<leader>B',
+        function()
+          dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = 'Debug: Set Breakpoint',
+      },
+      { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
+      unpack(keys),
+    }
+  end,
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    require('mason-nvim-dap').setup {
+      automatic_installation = true,
+      handlers = {},
+      ensure_installed = {
+      },
+    }
+    dapui.setup {
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    }
+
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    local elixir_ls_debugger = vim.fn.exepath "elixir-ls-debugger"
+    if elixir_ls_debugger ~= "" then
+      dap.adapters.mix_task = {
+        type = "executable",
+        command = elixir_ls_debugger,
+      }
+
+      dap.configurations.elixir = {
+        {
+          type = "mix_task",
+          name = "phoenix server",
+          task = "phx.server",
+          request = "launch",
+          projectDir = "${workspaceFolder}",
+          exitAfterTaskReturns = false,
+          debugAutoInterpretAllModules = false,
+        },
+      }
+    end
+  end,
+
+
 }
