18b059e1f2fd44c09482f0400ecd24933679c883
misdirection <misdirection@live.de>
2024-09-16 18:08:32 +0200

    added todo cemment keybinds

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,7 +2,16 @@ return {
   'folke/todo-comments.nvim', --colors TODO: commtents like that
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {}
+  config = function()
+    require('todo-comments').setup {}
+    vim.keymap.set("n", "]t", function()
+      require("todo-comments").jump_next()
+    end, { desc = "Next todo comment" })
+
+    vim.keymap.set("n", "[t", function()
+      require("todo-comments").jump_prev()
+    end, { desc = "Previous todo comment" })
+  end
 }
 
 --PERF: fully optimized
