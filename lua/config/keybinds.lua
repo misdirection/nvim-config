72d0e6787d75721c9daf1f9113ee7c9115246b4a
misdirection <misdirection@live.de>
2024-11-16 22:25:40 +0100

    fixed ESC

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -2,6 +2,4 @@ vim.keymap.set('n', 'WW', ':w<CR>', { noremap = true, silent = true })     -- CT
 vim.keymap.set('i', 'jj', '<ESC>', { noremap = true, silent = true })      -- jj to leave insert mode faster
 vim.keymap.set('n', '<C-c>', 'ggVG"+y', { noremap = true, silent = true }) -- CTRL + c to copy file to system clipboard
 vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })     -- copy selection (v) to system clipboard
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
--- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
