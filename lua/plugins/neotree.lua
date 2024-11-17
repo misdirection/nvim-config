e0906708617a74603351350087451e9c9c124b26
misdirection <misdirection@live.de>
2024-11-17 21:37:20 +0100

    changed keymaps

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -21,9 +21,8 @@ return {
   keys = {
     { '\\',         ':Neotree toggle current reveal_force_cwd<CR>', desc = 'Reveal temp explorer', { noremap = true, silent = true } },
     { '<Leader>ee', ':Neotree focus filesystem<CR>',                desc = 'focus explorer',       { noremap = true, silent = true } },
-    { '<Leader>ec', ':Neotree close<CR>',                           desc = 'close explorer',       { noremap = true, silent = true } },
-    { '<Leader>em', ':Neotree toggle show<CR>',                     desc = 'Toggle show',          { noremap = true, silent = true } },
-    { '<leader>eb', ':Neotree toggle show buffers right<CR>',       desc = 'Toggle Buffers right', { noremap = true, silent = true } },
-    { '<leader>es', ':Neotree float git_status<CR>',                desc = 'Floating git status',  { noremap = true, silent = true } },
+    { '<Leader>te', ':Neotree toggle show<CR>',                     desc = 'Toggle Explorer',      { noremap = true, silent = true } },
+    { '<leader>tb', ':Neotree toggle show buffers right<CR>',       desc = 'Toggle Buffers right', { noremap = true, silent = true } },
+    { '<leader>ts', ':Neotree float git_status<CR>',                desc = 'Floating git status',  { noremap = true, silent = true } },
   },
 }
