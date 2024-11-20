c9424875a063bb148f5c5f7e956c2a6a0e7d036e
Chris <christian.schmerl@planitprima.com>
2024-11-20 12:11:36 +0100

    feat(plugins): load plugins early and keymap for diffview open

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -67,6 +67,7 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons' },
+    event = "BufRead",
     keys = {
       { "<leader>jj", "<cmd>set hidden<cr><cmd>DiffviewClose<cr><cmd>set nohidden<cr>" },
     },
@@ -78,6 +79,7 @@ return {
       "sindrets/diffview.nvim",        -- optional - Diff integration
       "nvim-telescope/telescope.nvim", -- optional
     },
+    event = "BufRead",
     config = function()
       require('neogit').setup({
         commit_popup = {
@@ -87,6 +89,28 @@ return {
           diffview = true,
         },
       })
+      vim.keymap.set('n', '<leader>do', function()
+        -- Verwende Telescope, um Branches auszuwählen
+        require('telescope.builtin').git_branches({
+          attach_mappings = function(_, map)
+            map('i', '<CR>', function(prompt_bufnr)
+              local actions = require('telescope.actions')
+              local state = require('telescope.actions.state')
+              local selection = state.get_selected_entry(prompt_bufnr)
+              actions.close(prompt_bufnr)
+
+              -- Überprüfen, ob ein Branch ausgewählt wurde
+              if selection and selection.value then
+                -- Diffview öffnen mit dem Branch-Kontext
+                vim.cmd("DiffviewOpen " .. selection.value)
+              else
+                print("Abgebrochen: Kein Branch ausgewählt.")
+              end
+            end)
+            return true
+          end,
+        })
+      end, { noremap = true, silent = true, desc = "Diffview: Open diff for current file and branch" })
     end,
     keys = {
       { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit", noremap = true, silent = true },
